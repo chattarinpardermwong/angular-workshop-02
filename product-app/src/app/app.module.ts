13707e60e6c60b5789bd39d9d0094b700ad4871b
Chattarin Pardermwong <chattarinpar@mathregit.group>
2020-07-21 15:12:52 +0700

    route-successful

--- product-app/src/app/app.module.ts
+++ product-app/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +11,8 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
